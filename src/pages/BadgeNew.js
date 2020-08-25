bf4b359782806873a795c7611d796c469b333e16
Emanuel Catriquil <emacatriquil@gmail.com>
2020-08-25 19:29:02 -0300

    Se crean componentes para mostrar mensajes y se consume una api

--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -1,12 +1,15 @@
 import React from 'react';
 
 import './styles/BadgeNew.css'
-import header from '../images/badge-header.svg';
+import header from '../images/platziconf-logo.svg';
 import Badge from '../components/Badge';
 import BadgeForm from '../components/BadgeForm';
-
+import PageLoading from '../components/PageLoading';
+import api from '../api';
 class BadgeNew extends React.Component {
     state = {
+        loading: false,
+        error: null,
         form: {
             firstName: '',
             lastName: '',
@@ -31,27 +34,49 @@ class BadgeNew extends React.Component {
         })
     }
 
+    handleSubmit = async e => {
+        e.preventDefault();
+        this.setState({ loading: true, error: null });
+        try {
+            await api.badges.create(this.state.form);
+            this.setState({ loading: false });
+
+            this.props.history.push('/badges');
+        } catch (error) {
+            this.setState({ loading: false, error: error })
+        }
+    }
+
     render() {
+        if (this.state.loading) {
+            return <PageLoading />
+        }
         return (
             <React.Fragment>
                 <div className="BadgeNew__hero">
-                    <img className="img-fluid" src={header} alt="hero" />
+                    <img className="BadgeNew__hero-image img-fluid" src={header} alt="hero" />
                 </div>
 
                 <div className="container">
                     <div className="row">
                         <div className="col-6">
                             <Badge
-                                firstName={this.state.form.firstName}
-                                lastName={this.state.form.lastName}
+                                firstName={this.state.form.firstName || 'FIRST NAME'}
+                                lastName={this.state.form.lastName || 'LAST NAME'}
                                 avatarUrl="https://www.gravatar.com/avatar?d=identicon"
-                                jobTitle={this.state.form.jobTitle}
-                                twitter={this.state.form.twitter}
-                                email={this.state.form.email}
+                                jobTitle={this.state.form.jobTitle || 'JOB TITLE'}
+                                twitter={this.state.form.twitter || 'TWITTER'}
+                                email={this.state.form.email || 'EMAIL'}
                             />
                         </div>
                         <div className="col-6">
-                            <BadgeForm onChange={this.handleChange} formValues={this.state.form} />
+                            <h1>New Attendant</h1>
+                            <BadgeForm
+                                onChange={this.handleChange}
+                                onSubmit={this.handleSubmit}
+                                formValues={this.state.form}
+                                error={this.state.error}
+                            />
                         </div>
                     </div>
                 </div>
